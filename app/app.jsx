35c2f70e5c4a0656d95d0b938dcbddebf0e1be2f
Marius <marius.wium@redseed.com>
2018-10-26 11:30:31 +1300

    Some improvements to ApiNode component.

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 // import SWAPI from './components/explorer/Api.jsx';
 import ApiNode from './components/explorer/ApiNode.jsx';
 // import FilmsView from './components/explorer/filmsView.jsx';
-import Characters from './components/explorer/characters.jsx';
-import Starships from './components/explorer/starships.jsx';
+// import Characters from './components/explorer/characters.jsx';
+// import Starships from './components/explorer/starships.jsx';
 
 export default class StarWarsApp extends React.Component {
     state = {
@@ -22,7 +22,7 @@ export default class StarWarsApp extends React.Component {
         return (
             <div style={{ display: 'flex' }}>
                 <div style={{ flex: 1, padding: '0 1em' }}>
-                    <ApiNode endpoint="https://swapi.co/api/people/" />
+                    <ApiNode endpoint="https://swapi.co/api/" />
                 </div>
                 {this.state.selectedFilm && (
                     <div style={{ flex: 1, padding: '0 1em' }}>
--- app/components/explorer/ApiNode.jsx
+++ app/components/explorer/ApiNode.jsx
@@ -1,33 +1,74 @@
 import React from 'react';
 import Api from './Api.jsx';
 import { Loader, Grid, GridItem } from '../ui';
+import { isString, isPlainObject, isArray, isNumber, truncate } from 'lodash';
+import validUrl from 'valid-url';
 
 import withPromises from './withPromises.jsx';
 
 class ApiNode extends React.Component {
+    state = {
+        openNodes: []
+    };
+
+    clickHandler = (key, e) => {
+        e.stopPropagation();
+
+        let openNodes = [];
+        if (this.isOpen(key)) {
+            openNodes = this.state.openNodes.filter(item => item !== key);
+        } else {
+            openNodes = [...this.state.openNodes, key];
+        }
+        this.setState({ openNodes });
+    };
+
+    isOpen = key => this.state.openNodes.indexOf(key) !== -1;
+
+    nodeElement = (key, data) => {
+        if (!data) return null;
+        const isOpen = () => {
+            return this.isOpen(key);
+        };
+        const isEndpoint = () => {
+            return isString(data) && validUrl.isHttpsUri(data);
+        };
+        const isObjectNode = () => {
+            return isPlainObject(data);
+        };
+        const isArrayNode = () => {
+            return isArray(data);
+        };
+        return (
+            <GridItem key={key} onClick={e => this.clickHandler(key, e)}>
+                <h4>
+                    {key} <small>{typeof data}</small>
+                </h4>
+                {(isString(data) || isNumber(data)) && truncate(data)} <br />
+                {isOpen() && isEndpoint() && <ApiNode endpoint={data} />}
+                {isOpen() &&
+                    isObjectNode() && (
+                        <Grid show={true}>{Object.keys(data).map(key => this.nodeElement(key, data[key]))}</Grid>
+                    )}
+                {isOpen() &&
+                    isArrayNode() && <Grid show={true}>{data.map((item, i) => this.nodeElement(i, item))}</Grid>}
+            </GridItem>
+        );
+    };
+
     render() {
-        const { endpoint } = this.props;
         return (
             <div>
                 {this.props.loading && <Loader text="Loading data" />}
 
-                <Api endpoint={endpoint} key={endpoint} addPromise={this.props.addPromise}>
+                <Api endpoint={this.props.endpoint} key={this.props.endpoint} addPromise={this.props.addPromise}>
                     {({ loading, data }) => {
                         if (!data) {
                             return null;
                         } else {
                             return (
                                 <Grid show={!loading}>
-                                    {Object.keys(data.results).map(key => (
-                                        <GridItem key={key}>
-                                            <h3>{data.results[key].name}</h3>
-                                            <Grid show={!loading}>
-                                                {Object.keys(data.results[key]).map(key => (
-                                                    <GridItem key={key}>{key}</GridItem>
-                                                ))}
-                                            </Grid>
-                                        </GridItem>
-                                    ))}
+                                    {Object.keys(data).map(key => this.nodeElement(key, data[key]))}
                                 </Grid>
                             );
                         }
